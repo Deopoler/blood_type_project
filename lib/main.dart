7675659a04d592c8a429ff4356321f60d4c73823
deopoler <50035578+Deopoler@users.noreply.github.com>
2022-12-16 22:18:05 +0900

    Retitle

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '혈액형 프로젝트',
       theme: ThemeData(
         primarySwatch: Colors.lightGreen,
         fontFamily: 'NotoSansKR',
